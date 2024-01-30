0d3727777642d20015cc2a50f06f4ea00a209fc6
autarkist <autarkist@naver.com>
2024-01-31 00:08:07 +0900

    styling #3 small modifications

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -30,6 +30,14 @@ const Links = () => {
       {links.map((link) => (
         <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
     </div>
   );
 };
