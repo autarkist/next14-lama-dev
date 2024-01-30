70976394d3d7769bf2866393865c1df4ade5ce4c
Jaejin Yun <autarkist@naver.com>
2024-01-30 17:30:38 +0900

    styling #1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import Links from "./links/Links";
-
+import styles from "./navbar.module.css"
 const Navbar = () => {
   return (
     <div>
-      <div>Logo</div>
-      <div>
-        <Links />
+      <div className={styles.container}>
+        <div className={styles.logo}>Logo</div>
+        <div>
+          <Links />
+        </div>
       </div>
     </div>
   );
