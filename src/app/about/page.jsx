1e6004e35691bf8311a18c849779a2dfe3b311c2
autarkist <autarkist@naver.com>
2024-01-31 13:58:18 +0900

    image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,11 @@
+import Image from "next/image";
+import styles from "./about.module.css";
 const AboutPage = () => {
-  return <div>About Page!</div>;
+  return (
+    <div className={styles.imgContainer}>
+      <Image src="/img/chalet-5218666_1280.png" alt="" fill />
+    </div>
+  );
 };
 
 export default AboutPage;
