df6c56b2c5954c9a09195a10ce1b3c9d836194bc
Jaejin Yun <autarkist@naver.com>
2024-02-01 08:51:34 +0900

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
+import styles from "./home.module.css"
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return <div className={styles.container}>
+    <div className={styles.textContainer}></div>
+    <div className={styles.imgContainer}></div>
+  </div>;
 };
 
 export default Home;
