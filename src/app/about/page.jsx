19577db873c78a018ad75913c7925027d5f0c230
Jaejin Yun <autarkist@naver.com>
2024-01-31 14:13:12 +0900

    image #2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import styles from "./about.module.css";
 const AboutPage = () => {
   return (
     <div className={styles.imgContainer}>
-      <Image src="/img/chalet-5218666_1280.png" alt="" fill />
+      <Image src="/public/" alt="" fill />
     </div>
   );
 };
