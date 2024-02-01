4c8a7db07b1b8a9f6ec217eb2213ac8cd474d343
Jaejin Yun <autarkist@naver.com>
2024-02-01 11:47:25 +0900

    home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,30 @@
-import styles from "./home.module.css"
-import Image from "next/image"
+import styles from "./home.module.css";
+import Image from "next/image";
 
 const Home = () => {
-  return <div className={styles.container}>
-    <div className={styles.textContainer}>
-      <h1>Creative Thoughts Agency</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum hic quisquam officiis! Doloribus voluptate unde possimus fugiat laborum nesciunt ab illo sit eligendi inventore ad, quas vero tenetur earum laudantium.</p>
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum hic
+          quisquam officiis! Doloribus voluptate unde possimus fugiat laborum
+          nesciunt ab illo sit eligendi inventore ad, quas vero tenetur earum
+          laudantium.
+        </p>
+        <div className={styles.buttons}>
+          <div className={styles.button}>Learn More</div>
+          <div className={styles.button}>Contact</div>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" fill alt="" className={styles.brand} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image className={styles.heroImage} src="/hero.gif" fill alt="" />
+      </div>
     </div>
-    <div className={styles.buttons}>
-      <div className={styles.button}>Learn More</div>
-      <div className={styles.button}>Contact</div>
-    </div>
-    <div className={styles.brands}>
-      <Image src="/brands.png" fill alt="" className={styles.brand} />
-    </div>
-    <div className={styles.imgContainer}>
-      <Image className={styles.heroImage} src="/hero.gif" fill alt=""/>
-    </div>
-  </div>;
+  );
 };
 
 export default Home;
