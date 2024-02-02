92a5629d2a2780f3ee17790e0e84af66138cdf4f
autarkist <autarkist@naver.com>
2024-02-02 10:30:16 +0900

    single blog

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,40 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
-    return <div>Single Post Page!</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={"/static/moon.png"} alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src={"/static/girl.png"}
+            alt=""
+            className={styles.avartar}
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry Jefferson</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>2024.02.02.</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+          quam, assumenda itaque architecto omnis mollitia, non quod tenetur
+          blanditiis, ratione natus magnam minima? Autem eos nesciunt saepe
+          aspernatur, culpa quaerat.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
